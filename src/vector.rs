a1bcd783e995ea26912d61bfc82ae0969cdf2de1
Hannes Karppila <hannes.karppila@gmail.com>
2021-02-20 12:18:09 +0200

    Implement cross product

--- src/vector.rs
+++ src/vector.rs
@@ -36,6 +36,14 @@ impl Vector {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
+    pub fn cross(self, other: Self) -> Self {
+        Self {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
     /// https://math.stackexchange.com/a/13266/300156
     pub fn reflect(self, normal: Self) -> Self {
         self - normal * (2.0 * self.dot(normal) / self.len2())
@@ -91,3 +99,48 @@ impl Mul<float> for Vector {
 }
 
 pub type Point = Vector;
+
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    use super::Vector;
+
+    #[test]
+    fn cross_product_simple_0() {
+        let a = Vector {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        };
+
+        let b = Vector {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        };
+
+        let c = a.cross(b);
+
+        assert!(approx_eq(c.x, 0.0));
+        assert!(approx_eq(c.y, 0.0));
+        assert!(approx_eq(c.z, 1.0));
+    }
+
+    #[test]
+    fn cross_product_simple_1() {
+        let a = Vector { x: 0.0, y: 0.0, z: 0.5 };
+        let b = Vector { x: 0.0, y: 0.5, z: 0.0 };
+
+        let c = a.cross(b).normalized();
+        let d = b.cross(a).normalized();
+
+        assert!(approx_eq(c.x, -1.0));
+        assert!(approx_eq(c.y, 0.0));
+        assert!(approx_eq(c.z, 0.0));
+
+        assert!(approx_eq(d.x, 1.0));
+        assert!(approx_eq(d.y, 0.0));
+        assert!(approx_eq(d.z, 0.0));
+    }
+}
